29c6abc4c96ad6858e814b4b7efee4269d9f8b3c
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-07-25 15:43:44 +0900

    chromatic github actions test

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -3,5 +3,5 @@
 import * as React from "react";
 
 export const Button = () => {
-  return <button onClick={() => alert("button")}>Boop</button>;
+  return <button onClick={() => alert("버튼")}>Boop</button>;
 };
